0d205322adfd60b359a6cf2a252e72e25e3acdc4
Anatoli <anatoli.veselinov@gmail.com>
2017-10-16 14:49:55 +0200

    Extended generate.cmd

--- generate.cmd
+++ generate.cmd
@@ -1,2 +1,5 @@
-:: run generator
-"./scripts/genHtmlReportFromGtest.py" "out/gtest_report.html" "in/*.xml"
+cd input/
+unit_tests.exe --gtest_output=xml
+cd ..
+python genHtmlReportFromGtest.py "input/test_detail.xml" "output/gtest_report.html"
+start output\gtest_report.html
